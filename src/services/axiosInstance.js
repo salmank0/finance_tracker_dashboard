8f4a2edebf4e641c3a5fe9129f450672fd99c96a
Mohammad Salman Khan <salman.khan@enpointe.io>
2024-10-10 19:11:57 +0530

    Bug Fixes

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000", // Backend API base URL
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASEURL || "http://localhost:3000", // Backend API base URL
   headers: {
     "Content-Type": "application/json",
   },
